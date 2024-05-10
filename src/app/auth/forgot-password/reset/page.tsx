7325c40194ed8ac13ce64ce8d08c7a6ece3f63d1
Gaspard Delvaux <82412381+gasparddelvaux@users.noreply.github.com>
2024-05-10 21:02:18 +0200

    Forgot Password Reset part with Shadcn/UI

--- src/app/auth/forgot-password/reset/page.tsx
+++ src/app/auth/forgot-password/reset/page.tsx
@@ -1,22 +1,25 @@
 "use client";
 
 import AuthLayout from "@/app/layouts/AuthLayout";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import TWButton from "@/components/ui/forms/Button";
 import TWInput from "@/components/ui/forms/Input";
+import { Input } from "@/components/ui/input";
 import Logo from "@/components/ui/logo";
 import { useForm } from "react-hook-form";
 
 export default function AuthForgotPasswordResetPage() {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
+  const form = useForm();
 
   const onSubmit = (data: any) => {
     console.log(data);
-    reset();
   };
 
   return (
@@ -26,38 +29,35 @@ export default function AuthForgotPasswordResetPage() {
       <p className="text-slate-800 mb-6">
         Plus qu'à modifier votre mot de passe. Retenez-le, cette fois-ci!
       </p>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="grid grid-cols-1 gap-y-4 gap-x-2 mb-6"
-      >
-        <TWInput
-          type="password"
-          name="password"
-          label="Nouveau mot de passe"
-          errors={errors}
-          register={register}
-          validationSchema={{
-            required: "Le mot de passe est requis",
-            minLength: {
-              value: 6,
-              message: "Le mot de passe doit contenir au moins 6 caractères",
-            },
-          }}
-          required
-        />
-        <TWInput
-          type="password"
-          name="passwordConfirmation"
-          label="Confirmer le mot de passe"
-          errors={errors}
-          register={register}
-          validationSchema={{
-            required: "La confirmation du mot de passe est requise",
-          }}
-          required
-        />
-        <TWButton text="Modifier mon mot de passe" />
-      </form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nouveau mot de passe</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password_confirmation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirmer le nouveau mot de passe</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Changer le mot de passe</Button>
+        </form>
+      </Form>
     </AuthLayout>
   );
 }
